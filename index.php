ce890bae89ae737e1ce7af9bf7e29de96c19d5d7
Ricardo Braz <ricardofmb@live.com.pt>
2019-03-09 17:04:27 +0000

    Criamos select usuario por login e senha

--- index.php
+++ index.php
@@ -13,10 +13,30 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
 */
-
+/*Carrega um usuario
 $root = new Usuario();
 
 $root->loadbyId(4);
 
 echo $root;
+
+*/
+/*Carrega uma lista de usuarios
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+*/
+
+/*Carrega uma lista com um search
+
+$search = Usuario::search("jo");
+echo json_encode($search);
+
+*/
+
+//Carrega usuario usando login e senha
+
+$usuario = new Usuario();
+$usuario->login("jose", "asdadas");
+echo $usuario;
 ?>
